e157f1e70e11f1803e68033baeb75deda7cb1337
TanderCypher <alexander@nbay.no>
2026-02-02 09:59:17 +0100

    changed eslint.config.mjs so we could have a clean CI for jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,27 @@ import js from "@eslint/js";
 import globals from "globals";
 
 export default [
+  // App / backend code (Node)
   {
     files: ["**/*.{js,mjs,cjs}"],
+    ignores: ["**/*.test.js"],
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.commonjs
       }
-    },
-    rules: {
-      // kan strammes senere
     }
   },
+
+  // Jest test files
+  {
+    files: ["**/*.test.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  },
+
   js.configs.recommended
 ];
